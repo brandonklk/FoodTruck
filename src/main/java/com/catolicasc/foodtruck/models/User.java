59faf3b20f201712432ad2cc43acc530c48682e9
BRANDON MARCOS KLUCK <brandon.kluck@catolicasc.edu.br>
2019-05-13 20:48:34 -0300

    Integração de banco de dados

---
+++ src/main/java/com/catolicasc/foodtruck/models/User.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.catolicasc.foodtruck.models;
+
+/**
+ *
+ * @author guilherme
+ */
+public class User {
+    
+    private Integer id;
+    private String name;
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+}
---
+++ src/main/java/com/catolicasc/foodtruck/repositories/UserRepository.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.catolicasc.foodtruck.repositories;
+
+import com.catolicasc.foodtruck.ConnectionFactory;
+import com.catolicasc.foodtruck.models.User;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author guilherme
+ */
+public class UserRepository {
+    private Connection connection;
+    
+    public UserRepository() {
+        connection = new ConnectionFactory().getConnection();
+    }
+    
+    public User getUserById(Integer userId) {
+        try{
+            String sql = "SELECT ID, NAME, EMAIL FROM USERS WHERE ID = ?";
+            PreparedStatement selectStmt = connection.prepareStatement(sql);
+            selectStmt.setInt(1, userId);
+            ResultSet resultSet = selectStmt.executeQuery();
+            
+            User user = null;
+            
+            if(resultSet.first()) {
+                user = new User();
+            
+                int id = resultSet.getInt("ID");
+                String name = resultSet.getString("NAME");
+                String email = resultSet.getString("EMAIL");
+
+                user.setId(id);
+                user.setName(name);
+                user.setEmail(email);
+            }
+            
+            return user;
+            
+            
+        }catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+    
+    public ArrayList<User> getAllUsers() {
+        try{
+            ArrayList<User> users = new ArrayList<>();
+            
+            String sql = "SELECT ID, NAME, EMAIL FROM USERS";
+            Statement selectStmt = connection.createStatement();
+            ResultSet resultSet = selectStmt.executeQuery(sql);
+            while(resultSet.next()) {
+                int id = resultSet.getInt("ID");
+                String name = resultSet.getString("NAME");
+                String email = resultSet.getString("EMAIL");
+                
+                User user = new User();
+                user.setId(id);
+                user.setName(name);
+                user.setEmail(email);
+                
+                users.add(user);
+            }
+            
+            return users;
+        }catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+    
+    public User add(User user) {
+        try {
+            String sql = "INSERT INTO USERS (NAME, EMAIL) VALUES (?, ?)";
+            PreparedStatement insertStmt = connection.prepareStatement(sql);
+            insertStmt.setString(1, user.getName());
+            insertStmt.setString(2, user.getEmail());
+            insertStmt.executeUpdate();
+            insertStmt.close();
+            
+            sql = "SELECT LAST_INSERT_ID() AS ID";
+            Statement selectStmt = connection.createStatement();
+            ResultSet resultSet = selectStmt.executeQuery(sql);
+            while(resultSet.next()) {
+                Integer id  = resultSet.getInt("ID");
+                user.setId(id);
+            }
+            selectStmt.close();
+            
+            return user;
+        }catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+    
+}
